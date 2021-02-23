df51f2746b0bdd0a085e2f3812c8b7249fd14d7f
itw_yuzx01 <1098034155@qq.com>
2021-02-23 12:20:02 +0800

    初始文件

---
+++ src/main/java/com/wlxdw/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.wlxdw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(new Class[]{DemoApplication.class}, args);
+    }
+
+
+}
