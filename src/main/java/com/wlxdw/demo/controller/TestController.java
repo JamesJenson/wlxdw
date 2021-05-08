3fbd48c3772151ae9c21c6a2ec14dc18bb86a496
itw_yuzx01 <1098034155@qq.com>
2021-05-08 09:59:35 +0800

    测试类调整

--- src/main/java/com/wlxdw/demo/controller/TestController.java
+++ src/main/java/com/wlxdw/demo/controller/TestController.java
@@ -9,6 +9,7 @@ import com.wlxdw.demo.service.TestService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,11 +27,22 @@ public class TestController {
     @Autowired
     private TestService testService;
 
-    @PostMapping("/test")
+    @GetMapping("/test")
     @ApiOperation(value = "测试接口",notes = "测试接口")
-    public Rsp test(@RequestBody Req<Hahah> req){
-        System.out.println(JSONObject.toJSONString(req));
-        //testService.test("AAA");
-        return RspBuilder.builder().setRequsetId(req.getRequestId()).setRequestTime("20210224").build();
+    public String test(@RequestParam(name = "url") String url){
+        //String url = "https://cloud.tk.cn/tkpage/S20210026/#/?sourceType=19&utm_source=gzhmbxx5&idType=openId";
+        //String url = "https://cloud.tk.cn/tkpage/gift-common/#/?activityType=10";
+        url = Base64.encodeBase64URLSafeString(url.getBytes());
+        String oathUrl = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxa05747abe666b3b2&redirect_uri=http://wxpt.taikang.com/tkmap/wechat/oauth2/safeRedirect/wxa05747abe666b3b2?other=###&response_type=code&scope=snsapi_base&state=tkonline#wechat_redirect";
+        oathUrl = oathUrl.replace("###", url);
+        System.out.println(oathUrl);
+        System.out.println(oathUrl.length());
+
+        /*String oathUrl = WesureUtils.requestBodyDecrypt("1835C43E9A260A18666FAA5B8EF14A02");
+        System.out.println(str);*/
+
+        //System.out.println(Pattern.matches("^1\\d{10}$",""));
+
+        return oathUrl;
     }
 }
